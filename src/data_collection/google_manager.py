6f5e5a3744f4a589d8671dc5ef4e5f0bda47ffe6
unknown <riteshkp@hotmail.com>
2020-11-25 19:10:12 -0800

    google trend data added

--- src/data_collection/google_manager.py
+++ src/data_collection/google_manager.py
@@ -5,7 +5,7 @@ import pandas as pd
 from pytrends import dailydata
 
 if __name__ == "__main__":
-    df = dailydata.get_daily_data('Disney', 2018, 1, 2020, 11, geo = '')
+    df = dailydata.get_daily_data('Intel', 2018, 1, 2020, 11, geo = '')
     compression_opts = dict(method='zip', archive_name='out.csv')  
     df.to_csv('out.zip', index=False, compression=compression_opts)  
     print(df)
